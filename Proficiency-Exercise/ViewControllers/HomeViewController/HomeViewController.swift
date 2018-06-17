11e3aa3f6acfeae9359452c6268c107c67f972b0
Vasim Ajmeri <vasimajmeri12@gmail.com>
2018-06-17 14:07:38 +0530

    Parse data from API using NSURLConnection.

--- Proficiency-Exercise/ViewControllers/HomeViewController/HomeViewController.swift
+++ Proficiency-Exercise/ViewControllers/HomeViewController/HomeViewController.swift
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, NSURLConnectionDelegate, NSURLConnectionDataDelegate {
     
 
     @IBOutlet weak var tblViewHome: UITableView!
     
-    let cellIdentifier = "homeCell"
+    let CELL_IDENTIFIRE = "homeCell"
+    let API_URL         = "https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"
     
+    var responseData    = Data()
+
     
 //MARK: VIEW DELEGATES METHODS
 
@@ -23,6 +26,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         //setup tablview row height
         self.configTableView()
+        self.fetchDataFromAPI()
     }
     
     func configTableView(){
@@ -30,6 +34,61 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         self.tblViewHome.rowHeight          = UITableViewAutomaticDimension
     }
     
+    
+//MARK: FETCH DATA FROM API
+    
+    func fetchDataFromAPI(){
+        
+        let dataUrl        = URL.init(string: self.API_URL)
+        
+        if let dataUrl = dataUrl{//fetch optional value of url
+            let dataRequest    = URLRequest.init(url: dataUrl)
+            let dataConnection = NSURLConnection.init(request:dataRequest , delegate: self)
+            dataConnection?.start()
+        }
+    }
+
+    
+//MARK: CONNECTION DELEGATE METHODS
+ 
+    func connection(_ connection: NSURLConnection, didFailWithError error: Error) {
+        //Request failed
+        print(error.localizedDescription)
+    }
+    
+    func connection(_ connection: NSURLConnection, didReceive response: URLResponse) {
+        //New request, clear data
+        self.responseData = Data()
+    }
+    
+    func connection(_ connection: NSURLConnection, didReceive data: Data) {
+        //Append data while receive data
+        self.responseData.append(data)
+    }
+    
+    func connectionDidFinishLoading(_ connection: NSURLConnection) {
+        //convert data response into ISO latin
+        let responseEncode = String(data: self.responseData, encoding: .isoLatin1)
+        
+        //encode ISO latin to UT8
+        guard let responseDataUTF8 = responseEncode?.data(using: .utf8) else {
+            print("could not convert data to UTF-8 format")
+            return
+        }
+        
+        //parse data to json
+        do {
+            let jsonData = try JSONSerialization.jsonObject(with: responseDataUTF8) as? [String : Any]
+            
+            if let jsonData = jsonData{
+                print(jsonData)//parsed data
+            }
+            
+        } catch let jsonError {
+            print(jsonError)
+        }
+
+    }
     
 //MARK: TABLEVIEW DELEGATE METHODS
     
@@ -51,7 +110,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: self.cellIdentifier, for: indexPath) as! HomeViewCell
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_IDENTIFIRE, for: indexPath) as! HomeViewCell
         
         cell.lblTitle.text       = "My Title"
         cell.lblDescription.text = "My Description"
